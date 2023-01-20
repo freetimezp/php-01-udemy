0a9a8e55f13c9c7e0b1159d5f15cf99d108710fa
freetimezp <kraftukrnet@ukr.net>
2023-01-20 15:44:32 +0200

    05. php-01-udemy. Read and create table in db.

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,7 +5,8 @@ class Home extends Controller
     public function index()
     {
         $db = new Database();
-        $db->query();
+        $res = $db->query("SELECT * FROM users");
+        show($res);
 
         $data['title'] = "Home";
         $this->view('home', $data);
--- app/core/database.php
+++ app/core/database.php
@@ -2,7 +2,7 @@
 
 Class Database 
 {
-    public function db_connect() {
+    public function connect() {
         try {
             $string = DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";";
             return $db = new PDO($string, DB_USER, DB_PASS);
@@ -12,13 +12,48 @@ Class Database
         }
     }
 
-    public function query() {
-        $con = $this->db_connect();
-        show($con);
+    public function query($query, $data = [], $type = 'object') {
+        $con = $this->connect();
+        //show($con);
+
+        $stm = $con->prepare($query);
+        if($stm) {
+            $check = $stm->execute($data);
+            if($check) {
+                if($type == 'object') {
+                    $type = PDO::FETCH_OBJ;
+                }else{
+                    $type = PDO::FETCH_ASSOC;
+                }
+                $result = $stm->fetchAll($type);
+                if(is_array($result) && count($result) > 0) {
+                    return $result;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function create_tables() {
+        //users table
+        $query = "
+            CREATE TABLE IF NOT EXISTS `users` (
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `email` varchar(100) NOT NULL,
+                `password` varchar(255) NOT NULL,
+                `date` date DEFAULT NULL,
+                PRIMARY KEY (`id`),
+                KEY `email` (`email`),
+                KEY `date` (`date`)
+           ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+        ";
+
+        $this->query($query);
     }
 
     public function read($query, $data = []) {
-        $DB = $this->db_connect();
+        $DB = $this->connect();
         //show($DB);
         $stm = $DB->prepare($query);
 
@@ -40,7 +75,7 @@ Class Database
     }
 
     public function write($query, $data = []) {
-        $DB = $this->db_connect();
+        $DB = $this->connect();
         //show($DB);
         $stm = $DB->prepare($query);
 
